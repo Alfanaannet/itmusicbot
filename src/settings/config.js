4ff9a87fa55080ecb445e1b638d22595cb50de7f
Alfanaannet <134984185+Alfanaannet@users.noreply.github.com>
2024-03-31 03:11:06 +0200

    Update config.js

--- src/settings/config.js
+++ src/settings/config.js
@@ -25,9 +25,9 @@ module.exports = {
   nodes: [
     {
       name: process.env.NODE_NAME1 || "ELFANAAN LAVALINK", 
-      host: process.env.NODE_HOST1 || "lavalink.luminode.systems", 
+      host: process.env.NODE_HOST1 || "eu-lavalink.lexnet.cc", 
       port: parseInt(process.env.NODE_PORT1 || "443"), 
-      password: process.env.NODE_PASSWORD1 || "asyouwish", 
+      password: process.env.NODE_PASSWORD1 || "lexn3tl@val!nk", 
       secure: parseBoolean(process.env.NODE_SECURE1 || "true"),
     }
   ],
