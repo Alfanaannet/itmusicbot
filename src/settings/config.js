c6d4d36a08a472ba65bdd7df92048a9bb68584da
Alfanaannet <134984185+Alfanaannet@users.noreply.github.com>
2024-04-26 11:42:32 +0300

    Update config.js

--- src/settings/config.js
+++ src/settings/config.js
@@ -25,9 +25,9 @@ module.exports = {
   nodes: [
     {
       name: process.env.NODE_NAME1 || "ELFANAAN LAVALINK", 
-      host: process.env.NODE_HOST1 || "lavalink-v3.oryzen.xyz", 
+      host: process.env.NODE_HOST1 || "lava-v3.ajieblogs.eu.org", 
       port: parseInt(process.env.NODE_PORT1 || "80"), 
-      password: process.env.NODE_PASSWORD1 || "oryzen.xyz", 
+      password: process.env.NODE_PASSWORD1 || "https://dsc.gg/ajidevserver", 
       secure: parseBoolean(process.env.NODE_SECURE1 || "false"),
     }
   ],
